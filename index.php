26f855b72ea9870797fb2a697c3aabc93231ef28
Robert Mao <zhihong.mao@gmail.com>
2020-11-01 22:21:56 -0800

    initial adding files

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Blocklet Demo</title>
+ </head>
+ <body>
+ <h1>PHP is...</h1>
+ <?php echo '<p>The best language of the world to create Blocklet!</p>'; ?> 
+ </body>
+</html>
+
